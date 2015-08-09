cf61a7b94457707085377bd5c15bd29017186763
Todd Morrison <todd@deepelement.com>
2015-08-09 14:51:27 -0700

    Baseline

---
+++ README.md
@@ -0,0 +1,3 @@
+# browserify-bridge
+
+![Thank the knife](http://i.imgur.com/BZ5R5NP.png)
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+  browserify = require('gulp-browserify'),
+  clean = require('gulp-clean'),
+  runSequence = require('run-sequence');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
